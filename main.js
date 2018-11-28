1500913344de98cd0ad18b8b4e6cd1a9f59c8395
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-28 16:00:15 +0100

    pushmichele

--- main.js
+++ main.js
@@ -9,7 +9,19 @@ elementoverde.click(function(){
 });
 
 var elementorosso = $('.red');
+
 elementorosso.click(function(){
   $(this).css('background-color','red');
 
+});
+
+var punteggio = 0;
+elementoverde.click(function() {
+  punteggio++;
+  $("#counter").html("Hai selezionato " + punteggio + ' quadrati verdi');
+});
+var count = 0;
+elementorosso.click(function() {
+  punteggio++;
+  $("#counter2").html("Hai selezionato " + punteggio + ' quadrati rossi');
 });
