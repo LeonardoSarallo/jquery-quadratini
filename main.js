89cfe95bdb52e30abd4f60a4edcde7ffbd211965
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-28 14:54:48 +0100

    pushmichele

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // ￼Creare una griglia formata da 8x8 quadratini tutti bianchi.
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
-$('.quadrato').click(function(){
+
+var elemento = $('.quadrato');
+
+elemento.click(function(){
   $(this).css('background-color','green');
 });
